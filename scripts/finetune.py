d1760001fa00306ec01dec98260b83a74ea91a65
Matteo Kyle Santini <santini@izar1.izar.cluster>
2025-11-28 00:10:04 +0100

    Add Lora to finetune

--- scripts/finetune.py
+++ scripts/finetune.py
@@ -1,6 +1,7 @@
 import torch, wandb
-from dataclasses import asdict
+import loralib as lora
 
+from dataclasses import asdict
 from torch.utils.data import DataLoader
 
 from model_utils import load_model, batch_to_device
@@ -9,15 +10,18 @@ from embedings_utils import merge_datasets
 # Config
 wandb_run = None  # Set to wandb.run to log head training metrics to wandb
 
+# Model weights to start from
 pretrained_path = "model/ckpt.pt"
-out_dir = "model/finetuned_weights"
-batch_size = 32
-learning_rate = 1e-4
+# Where to save the finetuned model
+finetuned_path = "model/lora_32_5_labels.pt"
+
+batch_size = 64
+learning_rate = 1e-3
 weight_decay = 1e-3
-lora_r = 8  # LoRA rank
+lora_r = 32  # LoRA rank
 betas = (0.9, 0.999)
-num_epochs = 20
-labels = ["BCG_e1", "BCG_e2", "BCG_stellar_conc", "mass", "label"]
+num_epochs = 90
+label_names = ["BCG_e1", "BCG_e2", "BCG_stellar_conc", "mass", "label"]
 
 device = torch.device("cuda" if torch.cuda.is_available() else 
                       "mps" if torch.backends.mps.is_available() else 
@@ -38,7 +42,7 @@ if True:
             "batch_size": batch_size,
             "betas": betas,
             "lora_r": lora_r,
-            "labels": labels,
+            "labels": label_names,
             "epochs": num_epochs,
         },
     )
@@ -48,7 +52,14 @@ dataset = merge_datasets([
         "data/DarkData/BAHAMAS/bahamas_0.1.pkl",
         "data/DarkData/BAHAMAS/bahamas_0.3.pkl",
         "data/DarkData/BAHAMAS/bahamas_1.pkl",
-        "data/DarkData/BAHAMAS/bahamas_cdm.pkl"])
+        "data/DarkData/BAHAMAS/bahamas_cdm.pkl"]) \
+        .select_columns(["images", "images_positions", *label_names]) \
+        .shuffle(seed=42) \
+        .map(lambda row:  {
+                "images": row["images"],
+                "images_positions": row["images_positions"],
+                "labels": torch.stack([row[label] for label in label_names], dim=0)
+        })
 
 dataset = dataset.train_test_split(test_size=0.3, seed=42)
 train_dataset = dataset['train']
@@ -57,26 +68,39 @@ val_dataset = dataset['test']
 train_dl = DataLoader(
     train_dataset,
     batch_size=batch_size,
+    num_workers=4,
+    prefetch_factor=2,
     pin_memory=True,
     shuffle=True
 )
 
 val_dl = DataLoader(
     val_dataset,
-    batch_size=128,
+    batch_size=4*batch_size,
+    num_workers=4,
+    prefetch_factor=2,
     pin_memory=True,
     shuffle=False
 )
 
 # Load pretrained model
-model = load_model(pretrained_path, device, lora_r=lora_r, output_dim=len(labels))
+model = load_model(pretrained_path, device, lora_r=lora_r, output_dim=len(label_names))
+if lora_r > 0:
+    lora.mark_only_lora_as_trainable(model)
+else:
+    for param in model.parameters():
+        param.requires_grad = True
+
+# In any case, make sure task head is trainable
+for param in model.task_head.parameters():
+    param.requires_grad = True
 
 # Optimizer
 optimizer = model.configure_optimizers(
     weight_decay=weight_decay, learning_rate=learning_rate, 
     betas=betas, device_type=device)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 
-    T_max=num_epochs, eta_min=0, last_epoch=-1)
+    T_max=num_epochs, eta_min=1e-6, last_epoch=-1)
 
 # Training loop
 best_val_loss = float('inf')
@@ -84,16 +108,15 @@ for epoch in range(num_epochs):
 
     model.train()
     print(f"Training epoch {epoch}...")
-    epoch_train_loss = 0
+    train_loss = 0
     for B in train_dl:
 
         B = batch_to_device(B, device)
-        batch_labels = torch.stack([B[label] for label in labels], dim=1)
-        res, loss = model.get_task_prediction(B, targets=batch_labels)
+        res, loss = model.get_task_prediction(B, targets=B["labels"])
 
         loss.backward(retain_graph=False)
 
-        epoch_train_loss += loss.item() / len(train_dl)
+        train_loss += loss.item() / len(train_dl)
 
             
     optimizer.step()
@@ -102,35 +125,34 @@ for epoch in range(num_epochs):
 
     model.eval()
     print(f"Validating epoch {epoch}...")
-    epoch_val_loss = 0
+    val_loss = 0
     with torch.no_grad():
 
         for B in val_dl:
 
             B = batch_to_device(B, device)
-            batch_labels = torch.stack([B[label] for label in labels], dim=1)
-            res, loss = model.get_task_prediction(B, targets=batch_labels)
+            res, loss = model.get_task_prediction(B, targets=B["labels"])
 
-            epoch_val_loss += loss.item() / len(val_dl)
+            val_loss += loss.item() / len(val_dl)
 
-    print(f"Epoch {epoch}: val_loss {epoch_val_loss:.4f} and train_loss {epoch_train_loss:.4f}\n\n")
+    print(f"Epoch {epoch}: val_loss {val_loss:.4f} and train_loss {train_loss:.4f}\n\n")
     if wandb_run is not None:
         wandb_run.log({
-            "val_loss": epoch_val_loss, 
-            "train_loss": epoch_train_loss, 
+            "val_loss": val_loss, 
+            "train_loss": train_loss, 
             "learning_rate": optimizer.param_groups[0]['lr']
         })
 
     # Save best model
-    if epoch_val_loss < best_val_loss:
-        best_val_loss = epoch_val_loss
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
         torch.save({
             'model': model.state_dict(),
             'model_args': asdict(model.config),
             'modality_registry': model.modality_registry,
             'val_loss': best_val_loss,
-            'epoch_train_loss': epoch_train_loss, 
-        }, f"{out_dir}/best_model.pt")
+            'train_loss': train_loss, 
+        }, finetuned_path)
 
 if wandb_run is not None:
     wandb_run.finish()
