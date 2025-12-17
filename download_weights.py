af5fcc282452563c4dc7495855b6335aded8c3cb
Pijukai <julienbarozet@gmail.com>
2025-12-17 21:12:15 +0100

    fix : downloading script

--- download_weights.py
+++ download_weights.py
@@ -9,6 +9,6 @@ gdown.download_folder(folder_url, output='cache', quiet=False)
 # Move downloaded dataset to the appropriate directory
 shutil.move('cache/BAHAMAS', 'data')
 shutil.copytree('cache/MODEL_WEIGHTS', 'model_weights', dirs_exist_ok=True)
-os.rmdir('cache/MODEL_WEIGHTS')
+shutil.rmtree('cache/MODEL_WEIGHTS')
 
 print('All weights downloaded!')
