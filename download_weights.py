4046adad16121045fedc4c92ecea87f6de3d1020
Mattéo Santini <mattsantini2003@gmail.com>
2025-12-17 20:45:07 +0100

    Update download script

--- download_weights.py
+++ download_weights.py
@@ -1,8 +1,14 @@
 import gdown
 import os
+import shutil
 
 # Download entire folder from Google Drive
 folder_url = 'https://drive.google.com/drive/folders/1y1Wwj65rAHpNVW2cQMdUe-hwqwgwqVPa'
-gdown.download_folder(folder_url, output='model_weights', quiet=False)
+gdown.download_folder(folder_url, output='cache', quiet=False)
+
+# Move downloaded dataset to the appropriate directory
+shutil.move('cache/BAHAMAS', 'data')
+shutil.copytree('cache/MODEL_WEIGHTS', 'model_weights', dirs_exist_ok=True)
+os.rmdir('cache/MODEL_WEIGHTS')
 
 print('All weights downloaded!')
